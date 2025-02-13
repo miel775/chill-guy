56ea09315ae26ef9290954b49ff377e42a5b22c6
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-02-13 21:26:53 +0100

    Mugshot probleem
    Dacht mugshots te gebruiken in plaats van, githubavatars

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,2 @@
-// const year1971 = document.querySelectorAll('.y1971');
-// const bttn1971 = document.querySelector('.bttn1971');
-
-// const mugshots = document.querySelector('.mugshots');
-//     if (mugshots.classList.contains(.y1971);) {
-
-//     }
-//     else {
-//         return: null;
-//     }
+const year1971 = document.querySelectorAll('.y1971');
+const bttn1971 = document.querySelector('.bttn1971');
