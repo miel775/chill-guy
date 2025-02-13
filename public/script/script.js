f026bda879462290a946f9aa0e9173380b5ab28e
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-02-13 20:11:17 +0100

    Perfectionism it is.
    #3
    
    Ik snap er echt helemaal niks van, dus ga buttons maken en dan laat hij alle mensen zien van dat jaar.

---
+++ public/script/script.js
@@ -0,0 +1,10 @@
+// const year1971 = document.querySelectorAll('.y1971');
+// const bttn1971 = document.querySelector('.bttn1971');
+
+// const mugshots = document.querySelector('.mugshots');
+//     if (mugshots.classList.contains(.y1971);) {
+
+//     }
+//     else {
+//         return: null;
+//     }
